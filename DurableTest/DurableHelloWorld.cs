bb67caf4360549576781795336fde9015e3b2178
th3ragex <ragex@web.de>
2024-05-23 16:18:16 +0200

    Sample code

---
+++ DurableTest/DurableHelloWorld.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Microsoft.DurableTask.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace DurableTest;
+
+/// <summary>
+/// <see cref="https://learn.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-isolated-create-first-csharp?pivots=code-editor-vscode"/>
+/// </summary>
+static class DurableHelloWorld
+{
+    [Function(nameof(HelloCities))]
+    public static async Task<string> HelloCities([OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        // Works with Microsoft.Azure.Functions.Worker.Extensions.DurableTask v1.1.2, forever stuck with v1.1.3
+        var entityLock = new EntityInstanceId("HelloWorldLock", "helloworldlock");
+        await using (await context.Entities.LockEntitiesAsync(entityLock))
+        {
+            string result = "";
+            result += await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo") + " ";
+            result += await context.CallActivityAsync<string>(nameof(SayHello), "London") + " ";
+            result += await context.CallActivityAsync<string>(nameof(SayHello), "Seattle");
+            return result;
+        }
+    }
+
+    [Function(nameof(SayHello))]
+    public static string SayHello([ActivityTrigger] string cityName, FunctionContext executionContext)
+    {
+        ILogger logger = executionContext.GetLogger(nameof(SayHello));
+        logger.LogInformation("Saying hello to {name}", cityName);
+        return $"Hello, {cityName}!";
+    }
+
+    [Function(nameof(StartHelloCities))]
+    public static async Task<HttpResponseData> StartHelloCities(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [DurableClient] DurableTaskClient client,
+        FunctionContext executionContext)
+    {
+        ILogger logger = executionContext.GetLogger(nameof(StartHelloCities));
+
+        string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(nameof(HelloCities));
+        logger.LogInformation("Created new orchestration with instance ID = {instanceId}", instanceId);
+
+        return client.CreateCheckStatusResponse(req, instanceId);
+    }
+}
---
+++ DurableTest/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+    })
+    .Build();
+
+host.Run();
